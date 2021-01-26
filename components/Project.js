30d68317fa361f2edfaa1a74ccd98d68e5205809
svenvoskamp <sven.voskamp@student.howest.be>
2021-01-26 15:57:20 +0100

    fixing needs in projects

--- components/Project.js
+++ components/Project.js
@@ -41,22 +41,30 @@ const Project = ({ project, key }) => {
               <div className={`${style.info_fase} ${style.info_item}`}>
                 {project.phase.phase == 'Conceptvoorstel' && (
                   <>
-                    <div className={`${style.fase_color} ${style.fase_concept}`}></div>
+                    <div
+                      className={`${style.fase_color} ${style.fase_concept}`}
+                    ></div>
                   </>
                 )}
                 {project.phase.phase == 'Co-creatie' && (
                   <>
-                    <div className={`${style.fase_color} ${style.fase_creatie}`}></div>
+                    <div
+                      className={`${style.fase_color} ${style.fase_creatie}`}
+                    ></div>
                   </>
                 )}
                 {project.phase.phase == 'Crowdfunding' && (
                   <>
-                    <div className={`${style.fase_color} ${style.fase_crowdfunding}`}></div>
+                    <div
+                      className={`${style.fase_color} ${style.fase_crowdfunding}`}
+                    ></div>
                   </>
                 )}
                 {project.phase.phase == 'Realisatie' && (
                   <>
-                    <div className={`${style.fase_color} ${style.fase_realisatie}`}></div>
+                    <div
+                      className={`${style.fase_color} ${style.fase_realisatie}`}
+                    ></div>
                   </>
                 )}
                 <p className={`${style.info_text} ${style.info_bold}`}>
@@ -64,65 +72,118 @@ const Project = ({ project, key }) => {
                 </p>
               </div>
               <div className={`${style.info_location} ${style.info_item}`}>
-              <img src="./assets/images/project_location_icon.svg" />
+                <img src="./assets/images/project_location_icon.svg" />
                 <p className={`${style.info_text} ${style.info_light}`}>
                   {project.district.district}
                 </p>
               </div>
               <div className={`${style.info_needs} ${style.info_item}`}>
-
-
-              {project.needs.map((need) => (
-                <div className={style.need}>
-                  {need.type == 'Gebouw' && (
-                    <>
-                      <img
-                        className={style.need_image}
-                        src="./assets/images/gebouw_icon__small.svg"
-                      />
-                    </>
-                  )}
-                  {need.type == 'Eten' && (
-                    <>
-                      <img
-                        className={style.need_image}
-                        src="./assets/images/eten_icon__small.svg"
-                      />
-                    </>
-                  )}
-                  {need.type == 'Persoon' && (
-                    <>
-                      <img
-                        className={style.need_image}
-                        src="./assets/images/persoon_icon__small.svg"
-                      />
-                    </>
-                  )}
-                  {need.type == 'Item' && (
-                    <>
-                      <img
-                        className={style.need_image}
-                        src="./assets/images/item_icon__small.svg"
-                      />
-                    </>
-                  )}
-                  {need.type == 'Drank' && (
-                    <>
-                      <img
-                        className={style.need_image}
-                        src="./assets/images/drank_icon__small.svg"
-                      />
-                    </>
-                  )}
-                  <p className={`${style.info_text} ${style.info_light}`}>
-                    {need.need}
-                  </p>                
-                  </div>
-                ))}
-
+                {project.needs.length > 2 && (
+                  <>
+                    {project.needs.slice(0, 2).map((need) => (
+                      <div className={style.need}>
+                        {need.type == 'Gebouw' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/gebouw_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Eten' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/eten_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Persoon' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/persoon_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Item' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/item_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Drank' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/drank_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        <p className={`${style.info_text} ${style.info_light}`}>
+                          {need.need}
+                        </p>
+                      </div>
+                    ))}
+                    <p>+ {project.needs.length - 2}</p>
+                  </>
+                )}
+                {project.needs.length <= 2 && project.needs.length > 0 && (
+                  <>
+                    {project.needs.map((need) => (
+                      <div className={style.need}>
+                        {need.type == 'Gebouw' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/gebouw_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Eten' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/eten_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Persoon' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/persoon_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Item' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/item_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        {need.type == 'Drank' && (
+                          <>
+                            <img
+                              className={style.need_image}
+                              src="./assets/images/drank_icon__small.svg"
+                            />
+                          </>
+                        )}
+                        <p className={`${style.info_text} ${style.info_light}`}>
+                          {need.need}
+                        </p>
+                      </div>
+                    ))}
+                  </>
+                )}
               </div>
               <div className={`${style.info_crowdfunding} ${style.info_item}`}>
-              <img src="./assets/images/project_crowdfunding_icon.svg" />
+                <img src="./assets/images/project_crowdfunding_icon.svg" />
                 <p className={`${style.info_text} ${style.info_light}`}>
                   {project.district.district}
                 </p>
