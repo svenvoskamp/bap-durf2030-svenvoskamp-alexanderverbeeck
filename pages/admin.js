4160ad63fee2116d3de576c221112f179d01bd84
svenvoskamp <sven.voskamp@student.howest.be>
2021-02-02 21:26:42 +0100

    fixed viewport

--- pages/admin.js
+++ pages/admin.js
@@ -16,44 +16,44 @@ const Admin = ({ props }) => {
   return (
     <>
       <Mouse></Mouse>
-      <Nav user={props}></Nav>
+      {/* <Nav user={props}></Nav> */}
     </>
   );
 };
 
-const GetCurrentUser = ({ props }) => {
-  const router = useRouter();
-  const { loading, error, data } = useQuery(GET_CURRENT_USER, {
-    variables: { id: props.sub },
-  });
-  if (loading) {
-    return <Loading props={'gebruiker'} />;
-  }
-  if (error) {
-    console.log(error);
-  }
-  if (data.users[0].first_name) {
-    router.push('/');
-    return <></>;
-  }
-  return <Register props={data.users[0]} />;
-};
+// const GetCurrentUser = ({ props }) => {
+//   const router = useRouter();
+//   const { loading, error, data } = useQuery(GET_CURRENT_USER, {
+//     variables: { id: props.sub },
+//   });
+//   if (loading) {
+//     return <Loading props={'gebruiker'} />;
+//   }
+//   if (error) {
+//     console.log(error);
+//   }
+//   if (data.users[0].first_name) {
+//     router.push('/');
+//     return <></>;
+//   }
+//   return <Register props={data.users[0]} />;
+// };
 
-const getUser = () => {
-  const { user, loading } = useFetchUser();
-  const router = useRouter();
-  if (loading) {
-    return <Loading props={'gebruiker'} />;
-  }
-  if (!loading && !user) {
-    router.push('/');
-  }
-  if (!loading && user.sub != 'auth0|6019996f27e50e006cb10777') {
-    router.push('/');
-  }
-  if (!loading && user.sub == 'auth0|6019996f27e50e006cb10777')
-      return <GetAdminData user = {props}></GetAdminData>
-    }
-};
+// const getUser = () => {
+//   const { user, loading } = useFetchUser();
+//   const router = useRouter();
+//   if (loading) {
+//     return <Loading props={'gebruiker'} />;
+//   }
+//   if (!loading && !user) {
+//     router.push('/');
+//   }
+//   if (!loading && user.sub != 'auth0|6019996f27e50e006cb10777') {
+//     router.push('/');
+//   }
+//   if (!loading && user.sub == 'auth0|6019996f27e50e006cb10777')
+//       return <GetAdminData user = {props}></GetAdminData>
+//     }
+// };
 
-export default withApollo({ ssr: true })(getUser);
+export default withApollo({ ssr: true })(Admin);
