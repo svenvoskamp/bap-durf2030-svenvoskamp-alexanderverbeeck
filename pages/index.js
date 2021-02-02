d7154b5948b361b1139e9343c16d4995fc981a3d
svenvoskamp <sven.voskamp@student.howest.be>
2021-02-02 21:48:01 +0100

    fix samsung

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ const Home = ({ projects }) => {
       direction: 'horizontal',
       smartphone: {
         smooth: true,
-        gestureDirection: 'vertical',
+        horizontalGesture: true,
         direction: 'horizontal',
       },
     });
