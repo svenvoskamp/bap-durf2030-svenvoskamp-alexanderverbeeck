c755af5641ff9d14c0e4a50e571dfd7897a94cd8
Alexander Verbeeck <alexanderverbeeck@MacBook-Pro-van-Alexander.local>
2021-02-03 00:58:20 +0100

    Repsonsive Discoverpage

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,41 @@
-import { map, clamp } from '../lib/utils';
-import React, { useRef, useEffect } from 'react';
-import Tyle from '../components/Tyle/Tyle';
-import Mouse from '../components/Mouse';
-import gql from 'graphql-tag';
-import { withApollo } from '../lib/withApollo';
-import dynamic from 'next/dynamic';
-import Nav from '../components/Nav';
-import { useFetchUser } from '../lib/user';
+import { map, clamp } from "../lib/utils";
+import React, { useRef, useEffect } from "react";
+import Tyle from "../components/Tyle/Tyle";
+import Mouse from "../components/Mouse";
+import gql from "graphql-tag";
+import { withApollo } from "../lib/withApollo";
+import dynamic from "next/dynamic";
+import Nav from "../components/Nav";
+import { useFetchUser } from "../lib/user";
 
 const Home = ({ projects }) => {
   const { user, loading } = useFetchUser();
 
   useEffect(() => {
-    window.addEventListener('resize', reportWindowSize);
+    window.addEventListener("resize", reportWindowSize);
     const reportWindowSize = () => {
       if (window.innerWidth < 500) {
-        console.log('Mobile');
+        console.log("Mobile");
       }
     };
   });
   const scrollRef = useRef(null);
 
-  import('locomotive-scroll').then((locomotiveModule) => {
+  import("locomotive-scroll").then((locomotiveModule) => {
     const lscroll = new locomotiveModule.default({
       el: scrollRef.current,
       smooth: true,
-      direction: 'horizontal',
+      direction: "horizontal",
       smartphone: {
         smooth: true,
         horizontalGesture: true,
-        direction: 'horizontal',
+        direction: "horizontal",
       },
     });
 
-    lscroll.on('scroll', (obj) => {
+    lscroll.on("scroll", (obj) => {
       for (const key of Object.keys(obj.currentElements)) {
-        if (obj.currentElements[key].el.classList.contains('card')) {
+        if (obj.currentElements[key].el.classList.contains("card")) {
           let progress = obj.currentElements[key].progress;
           const brightnessVal =
             progress < 0.5
@@ -64,39 +64,41 @@ const Home = ({ projects }) => {
             <div className="home_start">
               <div className="home_text">
                 <div className="text_start">
+                  <div className="text-start--button">
+                    <p
+                      className="text-large--fill start-text--large--fill"
+                      data-scroll
+                      data-scroll-speed="5"
+                      data-scroll-direction="vertical"
+                    >
+                      {" "}
+                      ONTDEK.{" "}
+                    </p>
+                    <a
+                      href={"/"}
+                      className="button scale"
+                      data-scroll
+                      data-scroll-speed="8"
+                      data-scroll-direction="vertical"
+                    >
+                      <div className="circle_button">
+                        <img
+                          className="button_image"
+                          src="./assets/buttons/wat_durf_button.svg"
+                        />
+                      </div>
+                    </a>
+                  </div>
                   <p
-                    className="text-large--fill"
                     data-scroll
                     data-scroll-speed="5"
                     data-scroll-direction="vertical"
+                    className="text-large--inner start-text--large--inner"
                   >
-                    {' '}
-                    ONTDEK.{' '}
-                  </p>
-                  <p
-                    data-scroll
-                    data-scroll-speed="5"
-                    data-scroll-direction="vertical"
-                    className="text-large--inner"
-                  >
-                    {' '}
-                    DURF 2030.{' '}
+                    {" "}
+                    DURF 2030.{" "}
                   </p>
                 </div>
-                <a
-                  href={'/'}
-                  className="button scale"
-                  data-scroll
-                  data-scroll-speed="8"
-                  data-scroll-direction="vertical"
-                >
-                  <div className="circle_button">
-                    <img
-                      className="button_image"
-                      src="./assets/buttons/wat_durf_button.svg"
-                    />
-                  </div>
-                </a>
               </div>
               <div
                 className="home_start__info"
@@ -122,7 +124,7 @@ const Home = ({ projects }) => {
                       />
                     </picture>
                   </div>
-                  <a href={'/projects'} className="home_button__light scale">
+                  <a href={"/projects"} className="home_button__light scale">
                     Alle projecten
                   </a>
                 </div>
@@ -157,22 +159,22 @@ const Home = ({ projects }) => {
             <div className="home_end">
               <div className="text_end">
                 <p
-                  className="text-large--inner text-large--fill "
+                  className="text-large--fill end-text--large--fill"
                   data-scroll
                   data-scroll-speed="2"
                   data-scroll-direction="vertical"
                 >
-                  {' '}
-                  JOUW PROJECT.{' '}
+                  {" "}
+                  JOUW PROJECT.{" "}
                 </p>
                 <p
                   data-scroll
                   data-scroll-speed="-2"
                   data-scroll-direction="vertical"
-                  className="text-large--inner"
+                  className="text-large--inner end-text--large--inner"
                 >
-                  {' '}
-                  START NU.{' '}
+                  {" "}
+                  START NU.{" "}
                 </p>
               </div>
               <div
@@ -183,12 +185,12 @@ const Home = ({ projects }) => {
               >
                 <div className="home_end__buttons">
                   <a
-                    href={'/create-project'}
+                    href={"/create-project"}
                     className="home_button__start scale"
                   >
                     Start Nu
                   </a>
-                  <a href={'/projects'} className="home_button__light scale">
+                  <a href={"/projects"} className="home_button__light scale">
                     Alle projecten
                   </a>
                 </div>
@@ -202,8 +204,8 @@ const Home = ({ projects }) => {
 };
 
 export async function getStaticProps() {
-  const apollo = require('../lib/apolloClient'); // import client
-  var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+  const apollo = require("../lib/apolloClient"); // import client
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
   var xhr = new XMLHttpRequest();
   const GET_PROJECTS = gql`
     query getProjects {
