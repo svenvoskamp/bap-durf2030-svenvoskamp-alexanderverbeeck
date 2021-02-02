ab1a0f60d764f36f63ed85640f1c59bafb088510
Alexander Verbeeck <alexanderverbeeck@MacBook-Pro-van-Alexander.local>
2021-02-02 20:58:38 +0100

    Add Viewport meta

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap"
             rel="stylesheet"
