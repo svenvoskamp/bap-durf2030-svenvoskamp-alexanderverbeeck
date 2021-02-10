5928253093403ba308a3edb7ad45f227979d8b15
svenvoskamp <sven.voskamp@student.howest.be>
2021-02-10 17:39:46 +0100

    div change id detail

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,20 +1,20 @@
-import React, { useRef, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { gql, useQuery } from "@apollo/react-hooks";
-import { withApollo } from "../../lib/withApollo";
-import Mouse from "../../components/Mouse";
-import Header from "../../components/Detail/Header/Header";
-import Extra from "../../components/Detail/Extra/Extra";
-import Needs from "../../components/Detail/Needs/Needs";
-import Creatie from "../../components/Detail/Creatie/Creatie";
-import Crowdfunding from "../../components/Detail/Crowdfunding/Crowdfunding";
-import Realisation from "../../components/Detail/Realisation/Realisation";
-import { useFetchUser } from "../../lib/user";
-import gsap from "gsap";
+import React, { useRef, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { gql, useQuery } from '@apollo/react-hooks';
+import { withApollo } from '../../lib/withApollo';
+import Mouse from '../../components/Mouse';
+import Header from '../../components/Detail/Header/Header';
+import Extra from '../../components/Detail/Extra/Extra';
+import Needs from '../../components/Detail/Needs/Needs';
+import Creatie from '../../components/Detail/Creatie/Creatie';
+import Crowdfunding from '../../components/Detail/Crowdfunding/Crowdfunding';
+import Realisation from '../../components/Detail/Realisation/Realisation';
+import { useFetchUser } from '../../lib/user';
+import gsap from 'gsap';
 
-import Nav from "../../components/Nav";
-import style from "../../css/detail.module.css";
-import Loading from "../../components/Loading/Loading";
+import Nav from '../../components/Nav';
+import style from '../../css/detail.module.css';
+import Loading from '../../components/Loading/Loading';
 
 const GET_PROJECT_BY_ID = gql`
   query getProjectById($id: Int!, $user_id: String, $user: Boolean!) {
@@ -136,16 +136,16 @@ const Detail = ({ props, user, navUser }) => {
   const scrollRef = useRef(null);
   const [scrollNow, setScrollNow] = useState(false);
 
-  import("locomotive-scroll").then((locomotiveModule) => {
+  import('locomotive-scroll').then((locomotiveModule) => {
     let lscroll = new locomotiveModule.default({
       el: scrollRef.current,
       smooth: true,
-      direction: "horizontal",
+      direction: 'horizontal',
 
       smartphone: {
         smooth: true,
-        gestureDirection: "vertical",
-        direction: "vertical",
+        gestureDirection: 'vertical',
+        direction: 'vertical',
       },
     });
     if (scrollNow == true) {
@@ -155,11 +155,11 @@ const Detail = ({ props, user, navUser }) => {
 
   const onScroll = () => {
     if (
-      (props.projects[0].phase.phase == "Crowdfunding" &&
+      (props.projects[0].phase.phase == 'Crowdfunding' &&
         props.projects[0].reward_one) ||
-      props.projects[0].phase.phase == "Realisatie"
+      props.projects[0].phase.phase == 'Realisatie'
     ) {
-      const div = document.getElementsByTagName("html");
+      const div = document.getElementsByTagName('html');
       const el = document.querySelector(`.donations`);
 
       if (el.classList.contains(`is-inview`)) {
@@ -175,80 +175,80 @@ const Detail = ({ props, user, navUser }) => {
       }
     }
     if (
-      (props.projects[0].phase.phase == "Crowdfunding" &&
+      (props.projects[0].phase.phase == 'Crowdfunding' &&
         props.projects[0].reward_one) ||
-      props.projects[0].phase.phase == "Realisatie"
+      props.projects[0].phase.phase == 'Realisatie'
     ) {
-      console.log("ik moet nu wat doen");
+      console.log('ik moet nu wat doen');
       const crowdfundDiv = document.querySelector(`.progress_crowdfund`);
-      const el = document.querySelector(".crowdfund");
+      const el = document.querySelector('.crowdfund');
 
       if (el.classList.contains(`is-inview`)) {
-        if (crowdfundDiv.classList.contains(`progress_active`)) {
+        if (crowdfundDiv.classList.contains(`progress_active_crowdfund`)) {
         } else {
-          crowdfundDiv.classList.add(`progress_active`);
+          crowdfundDiv.classList.add(`progress_active_crowdfund`);
         }
       }
       if (!el.classList.contains(`is-inview`)) {
-        if (crowdfundDiv.classList.contains(`progress_active`)) {
-          crowdfundDiv.classList.remove(`progress_active`);
+        if (crowdfundDiv.classList.contains(`progress_active_crowdfund`)) {
+          crowdfundDiv.classList.remove(`progress_active_crowdfund`);
         }
       }
     }
 
     if (
-      props.projects[0].phase.phase == "Realisatie" &&
+      props.projects[0].phase.phase == 'Realisatie' &&
       props.projects[0].speech
     ) {
       const realisationDiv = document.querySelector(`.progress_realisation`);
-      const el = document.querySelector(".realisation");
+      const el = document.querySelector('.realisation');
 
       if (el.classList.contains(`is-inview`)) {
-        if (realisationDiv.classList.contains(`progress_active`)) {
+        if (realisationDiv.classList.contains(`progress_active_realisation`)) {
         } else {
-          realisationDiv.classList.add(`progress_active`);
+          realisationDiv.classList.add(`progress_active_realisation`);
         }
       }
       if (!el.classList.contains(`is-inview`)) {
-        if (realisationDiv.classList.contains(`progress_active`)) {
-          realisationDiv.classList.remove(`progress_active`);
+        if (realisationDiv.classList.contains(`progress_active_realisation`)) {
+          realisationDiv.classList.remove(`progress_active_realisation`);
         }
       }
     }
     const creationDiv = document.querySelector(`.progress_creation`);
-    const el = document.querySelector(".feedback");
+    const el = document.querySelector('.feedback');
 
     if (el.classList.contains(`is-inview`)) {
-      if (creationDiv.classList.contains(`progress_active`)) {
+      if (creationDiv.classList.contains(`progress_active_creation`)) {
       } else {
-        creationDiv.classList.add(`progress_active`);
+        creationDiv.classList.add(`progress_active_creation`);
       }
     }
     if (!el.classList.contains(`is-inview`)) {
-      if (creationDiv.classList.contains(`progress_active`)) {
-        creationDiv.classList.remove(`progress_active`);
+      if (creationDiv.classList.contains(`progress_active_creation`)) {
+        creationDiv.classList.remove(`progress_active_creation`);
       }
     }
 
     const conceptDiv = document.querySelector(`.progress_concept`);
-    const conceptEl = document.querySelector(".concept");
+    const conceptEl = document.querySelector('.concept');
 
     if (conceptEl.classList.contains(`is-inview`)) {
-      if (conceptDiv.classList.contains(`progress_active`)) {
+      if (conceptDiv.classList.contains(`progress_active_concept`)) {
       } else {
-        conceptDiv.classList.add(`progress_active`);
+        conceptDiv.classList.add(`progress_active_concept`);
       }
     }
     if (!conceptEl.classList.contains(`is-inview`)) {
-      if (conceptDiv.classList.contains(`progress_active`)) {
-        conceptDiv.classList.remove(`progress_active`);
+      if (conceptDiv.classList.contains(`progress_active_concept`)) {
+        conceptDiv.classList.remove(`progress_active_concept`);
       }
     }
   };
 
   useEffect(() => {
-    window.addEventListener("wheel", onScroll);
-  }, [window.removeEventListener("wheel", onScroll)]);
+    window.addEventListener('wheel', onScroll);
+  }, [window.removeEventListener('wheel', onScroll)]);
 
   return (
     <>
@@ -291,25 +291,25 @@ const Detail = ({ props, user, navUser }) => {
           </div>
           <Extra className={style.part_extra} props={props.projects[0]}></Extra>
           <div className={style.timeline}>
-            {" "}
-            {props.projects[0].phase.phase == "Co-creatie" && (
+            {' '}
+            {props.projects[0].phase.phase == 'Co-creatie' && (
               <div
                 className={`${style.timeline_line} ${style.timeline_line__cocreatie}`}
               ></div>
             )}
             {!props.projects[0].reward_one &&
-              props.projects[0].phase.phase == "Crowdfunding" && (
+              props.projects[0].phase.phase == 'Crowdfunding' && (
                 <div
                   className={`${style.timeline_line} ${style.timeline_line__waiting}`}
                 ></div>
               )}
             {props.projects[0].reward_one &&
-              props.projects[0].phase.phase != "Realisatie" && (
+              props.projects[0].phase.phase != 'Realisatie' && (
                 <div
                   className={`${style.timeline_line} ${style.timeline_line__crowdfunding}`}
                 ></div>
               )}
-            {props.projects[0].phase.phase == "Realisatie" && (
+            {props.projects[0].phase.phase == 'Realisatie' && (
               <div
                 className={`${style.timeline_line} ${style.timeline_line__realisatie}`}
               ></div>
@@ -318,7 +318,7 @@ const Detail = ({ props, user, navUser }) => {
             {props.projects[0].reward_one && (
               <Crowdfunding props={props} user={user}></Crowdfunding>
             )}
-            {props.projects[0].phase.phase == "Realisatie" && (
+            {props.projects[0].phase.phase == 'Realisatie' && (
               <Realisation
                 className={style.part_info}
                 props={props}
@@ -339,18 +339,18 @@ const LoadUser = ({ user }) => {
       variables: { user_id: user.sub, id: router.query.id, user: true },
     });
     if (loading) {
-      return <Loading props={"detail"} />;
+      return <Loading props={'detail'} />;
     }
     if (!data && !loading) {
-      router.push("/");
+      router.push('/');
       return <></>;
     }
     if (!data.projects[0] && !loading) {
-      router.push("/");
+      router.push('/');
       return <></>;
     }
-    if (data.projects[0].phase.phase == "Conceptvoorstel" && !loading) {
-      router.push("/");
+    if (data.projects[0].phase.phase == 'Conceptvoorstel' && !loading) {
+      router.push('/');
       return <></>;
     }
     if (data && !loading) {
@@ -359,21 +359,21 @@ const LoadUser = ({ user }) => {
   }
   if (!user) {
     const { loading, error, data } = useQuery(GET_PROJECT_BY_ID, {
-      variables: { user_id: "", id: router.query.id, user: false },
+      variables: { user_id: '', id: router.query.id, user: false },
     });
     if (loading) {
-      return <Loading props={"detail"} />;
+      return <Loading props={'detail'} />;
     }
     if (!data && !loading) {
-      router.push("/");
+      router.push('/');
       return <></>;
     }
     if (!data.projects[0] && !loading) {
-      router.push("/");
+      router.push('/');
       return <></>;
     }
-    if (data.projects[0].phase.phase == "Conceptvoorstel" && !loading) {
-      router.push("/");
+    if (data.projects[0].phase.phase == 'Conceptvoorstel' && !loading) {
+      router.push('/');
       return <></>;
     }
     if (data && !loading) {
@@ -385,7 +385,7 @@ const getUser = () => {
   const { user, loading } = useFetchUser();
 
   if (loading) {
-    return <Loading props={"detail"} />;
+    return <Loading props={'detail'} />;
   }
   if (!loading && user) {
     return <LoadUser user={user} />;
